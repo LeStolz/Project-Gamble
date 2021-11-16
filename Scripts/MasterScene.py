1f80acd0d6bd1f18c32306a8c9f085412d110133
LeStolz <vonamdang03@gmail.com>
2021-11-16 17:22:26 +0700

    Change game to g

--- Scripts/MasterScene.py
+++ Scripts/MasterScene.py
@@ -2,8 +2,8 @@ import pygame
 from Game import Game
 
 
-game = Game()
-# Abcsdddddddd.
-# Hello
-while game.running:
-	game.current_scene.draw_scene()
+g = Game()
+
+
+while g.running:
+	g.current_scene.draw_scene()
